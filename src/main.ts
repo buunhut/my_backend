3753d62f6e4262856e56d99c8944ceea55655db7
zero <zero@zeros-MacBook-Pro.local>
2023-09-22 12:41:17 +0700

    demo

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,26 @@ async function bootstrap() {
 
   //cấu hình swagger API
   const config = new DocumentBuilder()
-    .setTitle('Your API Title')
-    .setDescription('Your API Description')
-    .setVersion('1.0')
+    .setTitle('CYBERSOFT - NODEJS KHOÁ 33')
+    .setDescription('API nguoi-dung truyền vào headers token="HELLO"')
+    .setVersion('v1.0')
     // .addTag('your-tag') // Thêm các tag mô tả API của bạn
     .build();
   
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(8080);
 }
 bootstrap();
+
+
+//khởi tạo dự án nestjs bằng lệnh nest new tên dự án
+//test khởi chạy bằng yarn start:dev
+
+//cài prisma
+//yarn add prisma @prisma/client
+//yarn prisma init, chạy xong vào .env sửa thông tin chuỗi kết nối, vào schema.prisma sửa provider = "mysql"
+
+//yarm prisma db pull
+//yarn prisma generate
